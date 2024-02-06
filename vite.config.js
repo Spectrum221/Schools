418b174d19757fd24d13b6b8a167f24c03809d9f
Spectrum221 <desatonksqw@gmail.com>
2024-02-06 21:38:02 +0600

    first

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Schools/",
   plugins: [react()],
   optimizeDeps: {
     include: ['firebase/app'],
